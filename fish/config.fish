c822b55817601e272200131d75a773676519c0a8
minefuto <minefuto@gmail.com>
2020-10-22 13:52:05 +0900

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,8 @@
-set XDG_CONFIG_HOME ~/.config
-set PATH /usr/local/bin $PATH
-set PATH $HOME/.cargo/bin $PATH
-set PATH $HOME/.nimble/bin $PATH
+set -Ux XDG_CONFIG_HOME ~/.config
+set -Ux PYENV_ROOT $HOME/.pyenv
+set -Ux GOPATH $HOME/go
+set -Ux GOBIN $GOPATH/bin
+set -U fish_user_paths $GOBIN $PYENV_ROOT/bin $HOME/.nimble/bin /usr/local/bin $fish_user_paths
 
 # Fisher
 if not functions -q fisher
@@ -12,11 +13,11 @@ end
 
 # Base16 Shell
 if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
+  set BASE16_SHELL "$HOME/.config/base16-shell/"
+  source "$BASE16_SHELL/profile_helper.fish"
 end
 
-set -U EDITOR vim
+set -Ux EDITOR vim
 set -U pure_symbol_prompt ">"
 
 set -U fish_color_normal normal
@@ -46,3 +47,26 @@ set -U fish_pager_color_prefix white --bold --underline
 set -U fish_pager_color_progress brwhite --background=cyan
 
 alias vi='vim'
+
+# qwertycd 
+function qcd
+  qwertycd
+  cd (cat $HOME/.cache/qwertycd/cache_dir)
+end
+
+function transparent-pipeline
+  commandline | read -l buffer
+  commandline (tp -c "$buffer")
+end
+
+function fish_user_key_bindings
+  bind \ct transparent-pipeline
+end
+
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
+
+set -U fish_user_paths (echo $fish_user_paths | tr ' ' '\n' | sort -u)
